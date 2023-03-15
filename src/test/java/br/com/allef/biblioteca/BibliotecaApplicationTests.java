190c3f7a7b75b0edf526757b38b81e9ee7f30881
Allef Siqueira <allefsiqueira_6@hotmail.com>
2023-03-15 01:53:06 -0300

    Adicionando um banco h2 para teste

--- src/test/java/br/com/allef/biblioteca/BibliotecaApplicationTests.java
+++ src/test/java/br/com/allef/biblioteca/BibliotecaApplicationTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.*;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
+@ActiveProfiles("test")
 class BibliotecaApplicationTests {
 
 	private LivroService livroService;
